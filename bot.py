22730567828c6e526b5e9906192ad250f151a811
Shiv_10 <shivanshsharma2012@gmail.com>
2020-07-06 02:27:59 +0530

    fix: printing

--- bot.py
+++ bot.py
@@ -22,6 +22,7 @@ def get_data():
             challenges[data[i].strip()]=data[i+1:i+11]
             i+=11
         print(challenges)
+        print('\n\n\n\n')
         return "data recieved!"
     return "Hello Test!"
 
