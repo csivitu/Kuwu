5a6704b0cc3eafc0d17f0475334c945218d41838
Shiv_10 <shivanshsharma2012@gmail.com>
2020-07-08 23:11:42 +0530

    error: pushing code

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 #import json
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import os, asyncio
 from dotenv import load_dotenv
 import threading, time
@@ -12,6 +12,8 @@ app = Flask(__name__)
 data = []
 challenges={}
 monitor_data = {}
+
+
 @app.route('/', methods=['POST', 'GET'])
 def get_data():
     if request.method == 'POST':
@@ -25,6 +27,7 @@ def get_data():
             challenges[data[i].strip()]=data[i+1:i+11]
             i+=11
         monitor_data = challenges
+        print(monitor_data)
         print('data has been recieved!')
         return "data recieved!"
     return "Hello Test!"
@@ -36,11 +39,12 @@ def run_server():
 t1  = threading.Thread(target=run_server)
 t1.start()
 
-print('program continues!')
 client = commands.Bot(command_prefix='.')
 
 @client.event
 async def on_ready():
+    monitorChallenges.start()
+    challengeStatus.start()
     await client.change_presence(status =  discord.Status.online, activity=discord.Game('Type .list to list all commands'))
     print('Bot is ready')
 
@@ -49,18 +53,22 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-async def challengeStatus():
-    await client.wait_until_ready()
-    if (monitor_data == {}):
-        return
 
+@tasks.loop(seconds = 60)
+async def challengeStatus():
+
+    print('testing')
     statusChannel = client.get_channel(int(os.getenv('CHALLENGE_STATUS_CHANNEL')))
     w = ''
     table = [['TAG','Name', 'CPU %','MEM USAGE', 'MEM ALLOWED', 'MEM%']]
+
+    if monitor_data == {}:
+        return
+
     for i in dict.keys(monitor_data):
+        
         if (i == 'client_addr'):
             continue
-
         table.append([i,monitor_data[i][0], monitor_data[i][1], monitor_data[i][2], monitor_data[i][3], monitor_data[i][4]])
     
     for row in table:
@@ -69,34 +77,37 @@ async def challengeStatus():
     clientIp = monitor_data['client_addr']
     w += f'\n\nData recieved from I.P {clientIp}'
 
-    while True:
-        await statusChannel.send(w) #send challenge data here!
-        print(w)
-        time.sleep(60) #updates will be sent every minute
+    await statusChannel.send(w) #send challenge data here!
+    print(w)
 
+
+@tasks.loop(seconds = 15)
 async def monitorChallenges():
-    await client.wait_until_ready()
     
-    if (monitor_data == {}):
+    print(monitor_data)
+    if(monitor_data == {}):
         return
     
+    print('not returned')
+
     channel = client.get_channel(int(os.getenv('CHALLENGE_STATUS_CHANNEL')))
-    while True:
-        if (monitor_data == {}):
+
+
+    for i in dict.keys(monitor_data):
+        if (i == 'client_addr'):
             continue
 
-        for i in dict.keys(monitor_data):
-            if (i == 'client_addr'):
-                continue
+        if (float(monitor_data[i][1][0:-1]) > 50.00):
+            await channel.send(f'{monitor_data[i][0]} has a cpu usage of {monitor_data[i][1]}\nPlease check.\nTag: {i}')
 
-            if (float(monitor_data[i][1][0:-1]) > 50.00):
-                await channel.send(f'{monitor_data[i][0]} has a cpu usage of {monitor_data[i][1]}\nPlease check.\nTag: {i}')
+        if (float(monitor_data[i][4][0:-1]) > 60.00):
+            await channel.send(f'{monitor_data[i][0]} has a memory usage of {monitor_data[i][4]}\nPlease check.\nTag: {i}')
 
-            if (float(monitor_data[i][4][0:-1]) > 60.00):
-                await channel.send(f'{monitor_data[i][0]} has a memory usage of {monitor_data[i][4]}\nPlease check.\nTag: {i}')
-        time.sleep(30)
             
 
+@tasks.loop(seconds = 10)
+async def print_something():
+    print("hello")
 
 @client.event
 async def on_command_error(ctx, error):
@@ -121,21 +132,6 @@ async def on_command_error(ctx, error):
 async def firstBlood(userName, challengeName):
     channel = client.get_channel(int(os.getenv('FIRST_BLOOD_CHANNEL')))
     await channel.send(f'{userName} got first blood in challenge: {challengeName}')
-
-def creat_bg_task():
-    print('task created!')
-    client.loop.create_task(challengeStatus())
-
-def creat_monitor():
-    print('monitoring started')
-    client.loop.create_task(monitorChallenges())
-
-if (os.getenv('MODE') == 'PRODUCTION'):
-    t2 = threading.Thread(target=creat_bg_task)
-    t2.start()
-
-    t3 = threading.Thread(target=creat_monitor)
-    t3.start()
     
 
 
